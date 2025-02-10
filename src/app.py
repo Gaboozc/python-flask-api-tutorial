2e161e395854273ef84e2c11505a72e349679c0e
Gabriel Zavarse <zavarsegabriel@gmail.com>
2025-02-10 17:15:39 +0000

    flask

---
+++ src/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    print("Incoming request with the following body", request_body)
+    if 'label' in request_body and 'done' in request_body:
+        todos.append(request_body)
+        return jsonify(todos)
+    else:
+        return jsonify({"error": "Invalid todo format"}), 400
+    
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete:", position)
+    if 0 <= position < len(todos):
+        todos.pop(position)
+        return jsonify(todos)
+    else:
+        return jsonify({"error": "Invalid position"}), 400
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
